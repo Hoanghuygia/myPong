7da73571c66fa945160619ca0d41ca40ea2971c0
Hoanghuygia <hoanghuy051230@gmail.com>
2023-07-13 04:23:15 +0700

    calculate score

--- main.lua
+++ main.lua
@@ -24,7 +24,7 @@ function love.load()
 
     smallFont = love.graphics.newFont('font.ttf', 8)
     mediumFont = love.graphics.newFont('font.ttf', 16)
-    BigFont = love.graphics.newFont('font.ttf', 32)
+    bigFont = love.graphics.newFont('font.ttf', 32)
 
     gameState = 'startState'
 
@@ -38,6 +38,8 @@ function love.load()
     player2 = Paddle(VIRTUAL_WIDTH - 14, 97, 3, 15)
     ball = Ball(159, 105, 2)
 
+    
+
 end
 
 function love.keypressed(key)
@@ -45,6 +47,10 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == 'enter' or key == 'return' then--tại sao khi để enter thì nó không được mà để return thì nó lại okla ?
         if gameState == 'playState' then
+            gameState = 'serveState'
+        elseif gameState == 'serveState' then
+            gameState = 'startState'
+        elseif gameState == 'winState' then
             gameState = 'startState'
         else
             gameState = 'playState'
@@ -54,15 +60,34 @@ end
 
 function love.update(dt)
 
-    if gameState == 'playState' then
+    if gameState == 'serveState' then
         ball:update(dt)
         --reflect for the two width
         if (ball.y + ball.dy * dt) < 31 or (ball.y + ball.dy * dt) > 178 then
             ball.dy = -ball.dy
         end
 
+        --the case that the ball is outside the height border
+        if ball.x < 0 or ball.x > 320 then
+            if playerServe == 1 then
+                player1.score = player1.score + 1
+            else
+                player2.score = player2.score + 1
+            end
+            gameState = 'playState'
+            ball:resetLocation()
+            ball:resetSetup()
+            
+        end
+
+        --winer state
+        if player1.score == 10 or player2.score == 10 then
+            gameState = 'winState'
+        end
+
         --reflect for the paddle
         if ball:isCollide(player1) then
+            playerServe = 1
             ball.dx = -ball.dx * 1.1
             if ball.dy > 0 then
                 ball.dy = math.random(10,150)
@@ -73,18 +98,20 @@ function love.update(dt)
 
         if ball:isCollide(player2) then
             ball.dx = -ball.dx * 1.1
+            playerServe = 2
             if ball.dy > 0 then
                 ball.dy = math.random(10,150)
             else 
                 ball.dy = -math.random(10, 150)
             end
         end
-    elseif gameState == 'startState' then
+    elseif gameState == 'playState' then
+        if player1.dy ~= 0 or player2.dy ~= 0 then --add function that when paddle moves, it automatically transfer into play state
+            gameState = 'serveState'
+        end
+    else 
         ball:resetLocation()
         ball:resetSetup()
-        if player1.dy ~= 0 or player2.dy ~= 0 then --add function that when paddle moves, it automatically transfer into play state
-            gameState = 'playState'
-        end
     end
         
     --velocity for player1
@@ -116,6 +143,7 @@ function love.draw()
 
     --draw the field
     drawFiled()
+    displayScore()
 
     love.graphics.setFont(smallFont)
     love.graphics.print('State: '..gameState, 10, 10)
@@ -123,6 +151,19 @@ function love.draw()
     player1:render()
     player2:render()
     ball:render()
+
+    if gameState == 'winState' then
+        x = 60
+        y = 60
+        width = 50
+        height = 50
+        love.graphics.rectangle('fill', x, y, width, height) -- Vẽ hình chữ nhật
+        local text = "Hello" -- Nội dung chữ
+        local textX = x + (width - smallFont:getWidth(text)) / 2 -- Tính toán vị trí X cho chữ
+        local textY = y + (height - smallFont:getHeight()) / 2 -- Tính toán vị trí Y cho chữ
+
+        love.graphics.print(text, textX, textY) -- Viết chữ
+    end
 
     push:apply('end')
 
@@ -149,6 +190,14 @@ function drawFiled()
     -- --draw two dot
     -- love.graphics.rectangle('fill', 10, 105, 1, 1)
     -- love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, 105, 1, 1)
+end
+
+function displayScore()
+    love.graphics.setFont(bigFont)
+    love.graphics.print(tostring(player1.score), VIRTUAL_WIDTH / 2 - 30, 2)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 7, 15, 10, 2)
+    love.graphics.print(tostring(player2.score), VIRTUAL_WIDTH / 2 + 10, 2)
+    
 end
 
 
